27000aa1453a43727891a8db9ef1635620f8c271
siborion <siborion@localhost>
2014-02-07 21:43:59 +0700

    ????? ????????? ??????? ?? QWT

--- scanplot.h
+++ scanplot.h
@@ -12,6 +12,8 @@
 #include <qlayout.h>
 #include <qwt_plot_marker.h>
 #include <qwt_plot_curve.h>
+#include <QFileDialog>
+#include <QPushButton>
 
 class ScanPlot : public QWidget
 {
@@ -20,9 +22,17 @@ public:
     explicit ScanPlot(QWidget *parent = 0);
 
 private:
+    QGridLayout *layout;
+    QwtPlot *q_plot;
     QwtPlotMarker mX;
-    QwtPlotCanvas canvas;
-    QwtPlotCurve *d_curve1;
+//    QwtPlotCanvas *canvas;
+//    QwtPlotCurve *d_curve1;
+    QFileDialog *fileName;
+    QPushButton *pbTmp;
+    void getPlot(QwtPlot*);
+    double x[10];
+    double y[10];
+
 
 signals:
 
