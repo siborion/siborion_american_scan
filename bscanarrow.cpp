631860b541885291855f6b27529969ca5f35f3fa
siborion <siborion@localhost>
2016-02-24 17:18:09 +0600

    ????? ?????? ?? arrow?

--- bscanarrow.cpp
+++ bscanarrow.cpp
@@ -3,71 +3,19 @@
 BScanArrow::BScanArrow(quint16 x, quint16 y)
 {
     vertex.append(new BScanvertex(x,y));
-    start = new BScanvertex(x,y);
-    end = new BScanvertex(x,y);
-    editStart = editEnd = false;
-}
-
-void BScanArrow::setEnd(quint16 x, quint16 y)
-{
-    end->xKoord = x;
-    end->yKoord = y;
-}
-
-quint16 BScanArrow::getXStart()
-{
-    return start->xKoord;
-}
-
-quint16 BScanArrow::getYStart()
-{
-    return start->yKoord;
-}
-
-quint16 BScanArrow::getXEnd()
-{
-    return end->xKoord;
-}
-
-quint16 BScanArrow::getYEnd()
-{
-    return end->yKoord;
-}
-
-arrow_edit BScanArrow::getEdit()
-{
-    if(editStart)
-        return arrow_edit::START;
-    if(editEnd)
-        return arrow_edit::END;
-    return arrow_edit::NONE;
-}
-
-void BScanArrow::setEdit(arrow_edit val)
-{
-    switch(val)
-    {
-    case (arrow_edit::START):
-        editStart = true;
-        break;
-    case (arrow_edit::END):
-        editEnd = true;
-        break;
-    }
+    vertex.append(new BScanvertex(x+10,y+10));
 }
 
 BScanvertex *BScanArrow::findVertex(quint16 x, quint16 y)
 {
-    if(start->findVertex(x,y))
-        return start;
-    if(end->findVertex(x,y))
-        return end;
+    BScanvertex *vxTmp;
+    foreach (BScanvertex *vx, vertex)
+    {
+        vxTmp = vx->findVertex(x,y);
+        if(vxTmp)
+            return vxTmp;
+    }
     return 0;
-}
-
-void BScanArrow::resetEdit()
-{
-    editStart = editEnd = false;
 }
 
 
--- bscanarrow.h
+++ bscanarrow.h
@@ -8,27 +8,11 @@ enum arrow_edit{NONE, START, END};
 class BScanArrow
 {
 private:
-//    quint16 xStart, yStart, xEnd, yEnd;
-    bool editStart, editEnd;
 
 public:
     BScanArrow(quint16 , quint16 );
-
-    QList<BScanvertex> vertex;
-
+    QList<BScanvertex*> vertex;
     BScanvertex *findVertex(quint16 x, quint16 y);
-
-//    BScanvertex *start;
-//    BScanvertex *end;
-
-    quint16 getXStart();
-    quint16 getYStart();
-    quint16 getXEnd();
-    quint16 getYEnd();
-    arrow_edit  getEdit();
-    void setEnd(quint16 x, quint16 y);
-    void resetEdit();
-    void setEdit (arrow_edit);
 };
 
 #endif // BSCANARROW_H
