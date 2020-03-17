d2b1f3166b38fbaa39176f15920b05c58e0309b1
siborion <siborion@localhost>
2020-03-17 18:50:29 +0600

    ????? ?????

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "mainwindow.h"
 //#include <QSound>
 #include <QStyleFactory>
+#include <QInputDialog>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
@@ -11,9 +13,26 @@ int main(int argc, char *argv[])
     QCoreApplication::setLibraryPaths(paths);
     QApplication a(argc, argv);
 
-    //    a.setStyle(QStyleFactory::create("fusion"));
-    a.setStyle(QStyleFactory::create("windows"));
-    //    a.setStyle(QStyleFactory::create("windowsxp"));
+
+    qDebug()<<QStyleFactory::keys();
+
+    bool ok;
+//    QString text = QInputDialog::getText(0, ("QInputDialog::getText()"),
+//                                         ("User name:"), QLineEdit::Normal,
+//                                         QDir::home().dirName(), &ok);
+
+
+    QString textStyle = QInputDialog::getItem(0, ("Style"),
+                          ("Style:"),
+                          QStyleFactory::keys(), 0, true, &ok);
+
+
+    a.setStyle(QStyleFactory::create(textStyle));
+    //    a.setStyle(QStyleFactory::create("Fusion"));
+    //a.setStyle(QStyleFactory::create("windows"));
+    //        a.setStyle(QStyleFactory::create("WindowsXP"));
+    //a.setStyle(QStyleFactory::create("plastique"));
+     //a.setStyle(QStyleFactory::create("WindowsVista"));
 
 
     a.addLibraryPath(a.applicationDirPath()+"/plugins");
